51c4255fd329fa8d237206d218a3eb2046cf23ab
Leiner16 <leiner.mendoza@unillanos.edu.co>
2026-04-05 22:05:11 -0500

    feat: main layout with sidebar and dashboard view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,17 @@
 import { Routes } from '@angular/router';
 import { Login} from './pages/auth/login/login';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Dashboard } from './pages/dashboard/dashboard/dashboard';
+
 
 export const routes: Routes = [
     {path: 'login', component: Login},
-    {path: '', redirectTo: 'login', pathMatch:'full'}
+    { 
+        path: '', 
+        component: MainLayout,
+        children: [
+            { path: 'dashboard', component: Dashboard },
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+        ]
+    }
 ];
